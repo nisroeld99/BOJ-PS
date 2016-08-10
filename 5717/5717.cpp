cc4808d7219730847b41530281a6ed9ff5c7bbc5
최형규 <helloworld@choehyeong-gyuui-MacBook-Pro.local>
2016-08-10 15:20:16 +0900

    .

---
+++ 5717/5717.cpp
@@ -0,0 +1,25 @@
+/*
+ * 5717.cpp
+ */
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+typedef pair<int,int>ii;
+typedef pair<int,ii> iii ;
+typedef long long ll;
+const int inf = 987654321;
+const int mod = 0 ; 
+int main(){
+	while (true) {
+		int n , m ;
+		scanf("%d%d",&n,&m);
+		if (n==0 && m ==0) break;
+		printf("%d\n",n+m);
+	}
+
+	return 0 ;
+}
