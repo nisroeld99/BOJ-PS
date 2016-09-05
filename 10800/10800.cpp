ace0a7611c2cadc0bebe522920e027be804873e0
nisroeld99 <hellowworld1990@gmail.com>
2016-09-05 15:33:31 +0900

    sort

--- 10800/10800.cpp
+++ 10800/10800.cpp
@@ -1,41 +1,26 @@
-/*
- * 10800.cpp
- *
- */
- 
 #include <iostream>
-#include <cstring>
 #include <vector>
-#include <queue>
-#include <algorithm>
-using namespace std; 
-typedef pair<int,int> ii ;
-vector<ii> arr[2001];
+using namespace std;
+typedef pair<int,int> ii;
 int main() {
- 
     int n ;
     scanf("%d",&n);
-    for (int i =1 ; i<=n ; i++ ) {
-        int a,b;
-        scanf("%d%d",&a,&b) ;
-        arr[b].push_back(ii(a,i));
+    vector<ii> a[2001];
+    for (int i = 1 ; i<=n ; i++ ) {
+        int color, size ;
+        scanf("%d%d",&color,&size);
+        a[size].push_back({ color,i});
     }
-    vector<int> c (n+1,0);
-    vector<int> dab (n+1, 0);
-     
-    long long sum = 0 ; 
-    for (int i= 2; i<= 2000 ; i++ ) {
-        sum+= arr[i-1].size() * (i-1) ; 
-        for (int j=0 ; j< arr[i-1].size() ;j++ ){
-            c[arr[i-1][j].first ] += i-1;  
-        }
-        for (int j=0 ; j< arr[i].size() ;j++ ) {
-            dab[arr[i][j].second] = sum - c[arr[i][j].first ];  
-        }
-         
+    vector<int> c(n+1,0);
+    vector<int> dab(n+1,0);
+    int sum = 0 ;
+    for (int i = 2;  i<= 2000; i++ ) {
+        sum += a[i-1].size() * (i-1);
+        for (int j= 0 ; j< a[i-1].size() ; j++ )
+            c[ a[i-1][j].first  ] += i-1;
+        for (int j= 0 ; j< a[i].size() ;j++ )
+            dab[ a[i][j].second ] = sum-c[ a[i][j].first  ];
     }
-    for (int i = 1; i<=n;  i++ ) 
-        printf("%d\n",dab[i] ) ;
- 
-    return 0  ;
+    for (int i =1 ; i<=n; i++)
+        printf("%d\n",dab[i]);
 }
