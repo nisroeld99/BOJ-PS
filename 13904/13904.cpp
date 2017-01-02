cdcbf2fd43c2a347fdf14b08c59fabc3d838e086
nisroeld99 <hellowworld1990@gmail.com>
2017-01-02 10:22:51 +0900

    sort

---
+++ 13904/13904.cpp
@@ -0,0 +1,25 @@
+/*
+ * 13904.cpp
+ *
+ *  1. DEADLINE 기준 내림차순  정렬한다. 
+ *  2. 최대 많이 남은 시간부터 left_time 을 줄여나간다.( 여유가 많은것부터 처리)
+ *  3. 2번의 loop 안에서 while문은 deadline이 같은 것 처리 위해서  
+ *
+ *
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std; 
+
+int main() {
+
+	
+
+
+	return 0  ;
+}
