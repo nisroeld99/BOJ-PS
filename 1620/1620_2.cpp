8e0a389bcfbf0b97f1845abc1bf2c40897eb9cd3
최형규 <nisroeld99@gmail.com>
2017-02-03 00:46:23 +0900

    acc

--- 1620/1620_2.cpp
+++ 1620/1620_2.cpp
@@ -1,37 +1,43 @@
 /*
  *  1620_2   
  */
+
+
+/*
+ *  1620_2
+ */
 #include <cstdio>
 #include <string>
-#include <unordered_map>
+#include <cstring>
+#include <map>
 using namespace std;
 
 int n, m;
 char inp[25];
 string s[100005];
-unordered_map<string, int> r;
+map<string, int> r;
 
 void proc() {
-	scanf("%d %d", &n, &m);
-	for (int i = 1; i <= n; ++i) {
-		scanf("%s", inp);
-		s[i].assign(inp);
-		r[s[i]] = i;
-	}
-	for (int i = 0; i < m; ++i) {
-		scanf("%s", inp);
-		if ('0' <= inp[0] && inp[0] <= '9') {
-			int v;
-			sscanf(inp, "%d", &v);
-			printf("%s\n", s[v].c_str());
-		}
-		else {
-			printf("%d\n", r[inp]);
-		}
-	}
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; ++i) {
+        scanf("%s", inp);
+        s[i] = inp;
+        r[s[i]] = i;
+    }
+    for (int i = 0; i < m; ++i) {
+        scanf("%s", inp);
+        if ('0' <= inp[0] && inp[0] <= '9') {
+            int v;
+            v= stoi(inp); // string.h 
+            printf("%s\n", s[v].c_str());
+        }
+        else {
+            printf("%d\n", r[inp]);
+        }
+    }
 }
 
 int main() {
-	proc();
-	return 0;
+    proc();
+    return 0;
 }
