d46a7407109db9ae81d980c6ce880d97763f1cd3
nisroeld99 <hellowworld1990@gmail.com>
2017-02-02 16:59:38 +0900

    acc

--- 2959/2959.cpp
+++ 2959/2959.cpp
@@ -11,6 +11,10 @@
 using namespace std; 
 
 int main() {
+	/*
+	 * 가장 큰 사각형 만드려면 , 첫번쨰, 3번째 길이가 각각의 변이 되어야한다.   여러가지 방법이 있지만, 배운 stl을 써먹자면 , 
+	 *  sort해서 , v[0] , v[2] 값을 곱해줌 
+	 * */
 	vector<int>v;
 	int a,b,c,d;
 	scanf("%d%d%d%d", &a,&b,&c,&d);
