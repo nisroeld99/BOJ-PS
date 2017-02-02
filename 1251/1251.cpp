2fc01d27e5d72276d3a16305156a0a769e5c053b
hg <hellowworld1990@gmail.com>
2017-02-02 17:30:54 +0900

    Update 1251.cpp

--- 1251/1251.cpp
+++ 1251/1251.cpp
@@ -1,3 +1,5 @@
+
+
 #include<algorithm>
 #include <iostream>
 #include<vector>
@@ -9,6 +11,17 @@ string inputS;
  
  
 int main(){
+ 
+    /*
+      n = 50 이므로 , 전탐색으로 O(n^3) 으로 구할수 있다. (모든 경우의 수   따져보기) 
+      i ,k j 는 각각 경계선에 해당한다. 
+      경계선부터 revese 하여 temp string에 저장해서 dab과 비교해주는 것을 반복한다.
+      
+      dab 은 zzzzzzzz로 초기화 함으로써 가장 사전 상 뒤에 있는 것으로 초기화 
+      
+    
+    */
+ 
     cin>>inputS;
      
     int len= (int)inputS.size();
