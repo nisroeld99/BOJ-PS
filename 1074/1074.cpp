aa762a2f8d63f568def48a0a5bf5b4cc176a5262
nisroeld99 <hellowworld1990@gmail.com>
2017-02-07 09:23:39 +0900

    acc

--- 1074/1074.cpp
+++ 1074/1074.cpp
@@ -1,38 +1,38 @@
 /*
- *  1074   
+ * 1074.cpp
+ *
  */
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-using namespace std;
-typedef long long ll ;
-typedef pair<int,int> ii;
-typedef pair<int, ii> iii;
+#include <cstring>
+#include <stack>
+#include <queue>
+
+using namespace  std;
 
 
-ll go (ll n, ll r, ll c){
-    if (n==0 )return 0;
-    ll k = 1<<(n-1);
-    if ( r < k){
-        if ( c < k) return go(n-1,r,c);
-        else return go(n-1,r,c-k) +k*k ;
-    }
-    else {
-        if (c<k) return go(n-1,r-k,c)+k*k*2;
-        else return go(n-1,r-k,c-k)+k*k*3;
+int solve (int n, int r, int c){
+    if ( n == 0 ) return 0 ;
+    int k = (1<<(n-1));
+    if ( r<k){
+        if ( c <k) return solve ( n-1 , r,c );
+        else return solve (n-1, r,c-k) + k*k ;
+    }else {  // r 줄여줘야함
+        if ( c<k ) return solve (n-1,r-k,c) + k*k*2;
+        else  return solve (n-1,r-k, c-k) +k*k*3;
     }
     
 }
 
 
-int main(){
-    ll n,r,c;
-    while (scanf("%lld%lld%lld",&n,&r,&c ) !=EOF){
-        printf("%lld\n",go(n,r,c));
-    }
+int main(void){
+    /*
+       모든 경우는 항상 4등분되어 결과가 나타남
+       이러한 규칙을 이용해 재귀적인 방법으로 해결한다.
+     */
     
-    
+    int n , r, c;
+    while (scanf("%d%d%d",&n,&r,&c) != EOF  )
+        printf("%d\n",solve(n,r,c));
 }
