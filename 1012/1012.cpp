7e6d723900159bfffcab2a19656826d37c6c1844
nisroeld99 <hellowworld1990@gmail.com>
2017-02-07 13:26:22 +0900

    acc

--- 1012/1012.cpp
+++ 1012/1012.cpp
@@ -1,57 +1,57 @@
 /*
  * 1012.cpp
+ *
  */
 
 
-nclude <iostream>
-#include <stack>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <iostream>
+
 using namespace std;
 
-typedef long long  ll;
-const int inf =987654321;
-int dy[4]= {0,0,-1,1};
-int dx[4]= {1,-1,0,0};
-int g[55][55];
-int m, n ,k ;
-void go (int ypos,int xpos ) {
-    g[ypos][xpos] = 0 ;
-    for (int i=0;i<4; i++){
-        int ny= ypos + dy[i];
-        int nx= xpos+dx[i];
-        if (ny< 0 || nx< 0 || ny>= n || nx >=m )continue;
-        if(g[ny][nx]) go(ny,nx);
-        else continue;
+bool g[55][55];
+
+int dy[4] = {0,0,-1,1}, dx[4] ={1,-1,0,0};
+
+int x, y,k;
+
+void go ( int ypos , int xpos ){   //
+    g[ypos][xpos] = false ;
+    
+    for (int i = 0 ; i<4 ; i++){  //상하좌우를 dy,dx , ny,nx 로 표현함
+        int ny = ypos + dy[i];
+        int nx = xpos + dx[i];
+        if ( ny < 0|| nx< 0 || ny >=y || nx >= x ) continue; //g [][] 범위 이탈시,
+        if ( g[ny][nx] == false )continue;
+        go (ny,nx);
     }
-    
-}
-int main() {
-    
-    int tc;
-    cin>> tc;
-    
-    
-    while (tc-- ){
-        memset (g ,0,sizeof(g));
-        
-        scanf("%d%d%d",&m,&n,&k);
-        for (int i=0 ; i<k ; i++){
-            int a,b;
-            scanf("%d%d",&a,&b); // x,y축
-            g[b][a] = 1 ;
-        }
-        int cnt = 0 ;
-        for (int i = 0 ; i<n ; i++)
-            for (int j=0 ;j<m; j++){
-                if (g[i][j]){
-                    cnt ++ ;
-                    go(i,j);
-                }
-            }
-        printf("%d\n",cnt);
-    }
-    
 }
 
+
+int main(){
+    
+    
+    
+    int tc;
+    cin >> tc;
+    
+    while (tc--){
+        memset (g,false, sizeof(g)) ;  //기존의 g[][] 그래프를 모두 false로 초기화 #include <cstring>
+        
+        scanf("%d%d%d",&x,&y,&k);  //
+        while (k--){
+            int xx,yy;
+            scanf("%d%d",&xx,&yy);
+            g[yy][xx] = true ;  //배추가 심어진 위치 g[][] 그래프에 넣기
+        }
+        int ans = 0 ;  //배추 지렁이 개수
+        for (int i = 0 ; i< y; i++){
+            for (int j= 0 ;j<x ;j++){
+                if ( g[i][j] == true ){  // 다 돌면서 , 배추만나면
+                    ans ++ ;             // 지렁이 추가
+                    go ( i,j );          // 그리고 주변 배추도 이번에 추가된 지렁이가 다 먹으므로, dfs로 주변을 추적함
+                }
+            }
+        }
+        printf("%d\n",ans);
+    }
+}
