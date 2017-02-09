a86667028916b65bbbf9c6cb2e568c62049181e3
nisroeld99 <hellowworld1990@gmail.com>
2017-02-09 11:10:56 +0900

    acc

--- 4256/4256.cpp
+++ 4256/4256.cpp
@@ -21,12 +21,12 @@ int find_(int*arr, int root, int n){
 }
 
 void postorder(int*preorder , int*inorder, int n ){
-    int root = find_(inorder, preorder[0] , n ) ;
-    if ( root != 0 ){
+    int root = find_(inorder, preorder[0] , n ) ;  // inorder상에서 root를 찾아 . root왼쪽은 왼쪽tree, 오른쪽은 오른쪽tree
+    if ( root != 0 ){  //가장왼쪽 끝갈때까지, 이동, ( preorder +1 하는 이유는, 이게좌측이동이랑 같다. )  0이면 가장 좌측아래
         postorder(preorder+1, inorder , root);
     }
     
-    if (root != n-1 ) { //남아 있으면,
+    if (root != n-1 ) { //  우측 아래갈떄까지 조사,  root+1 하는 이유? root(1개) + 좌축 tree 제외한 우측 트리 추츨 root가
         postorder(preorder+root+1 , inorder+root+1, n-root-1) ;
     }
     printf("%d ", preorder[0] ) ;
