b46f72c040747ac2f530e964c620fd2994867b2f
최형규 <nisroeld99@gmail.com>
2017-02-19 23:12:01 +0900

    acc

--- 1890/1890.cpp
+++ 1890/1890.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <cstring>
-using namespace std;
 
-int a[111][111];
-int d[111][111];
-int dy[2] = {1,0} ;
-int dx[2] = {0,1};
+#include <iostream>
+using namespace std;
+typedef long long ll;
 int n ;
-int go (int ypos , int xpos){
-    if ( ypos == n && xpos == n) {
-        return 1;
-    }
-    int & ret = d[ypos][xpos] ;
-    if (ret>0 ) return  ret;
+int a[111][111];
+ll d[111][111];
+int dy[2] = {0,1};
+int dx[2] = {1,0};
+ll solve (int ypos , int xpos ){
+    ll &ret = d[ypos][xpos];
+    if (ret !=-1) return ret;
     
-    for (int i = 0 ; i<2; i++){
-        int ny = a[ypos][xpos]* dy[i] + ypos ;
-        int nx  = a [ypos][xpos] *dx[i] +xpos;
-        if  (ny <1 || nx < 1 || ny > n || nx > n ) continue;
-        ret+=go (ny,nx);
+    if ( ypos == n && xpos ==n ) return 1;
+    
+    ret = 0 ;
+    for (int i = 0 ; i <2 ;i++){
+        int ny = dy[i]*a[ypos][xpos] + ypos;
+        int nx = dx[i]*a[ypos][xpos] + xpos;
+        if (ny > n || nx > n )continue;
+        ret += solve(ny,nx);
     }
     return ret ;
 }
 
-int main(){
-    memset ( d, 0,sizeof(d));
-    scanf("%d",&n);
-    for (int i =1 ; i<=n ; i++){
+
+int main() {
+    cin >> n;
+    for (int i = 1 ; i<=n ; i++){
         for (int j=1 ;j<=n; j++){
             scanf("%d",&a[i][j]);
         }
     }
+    memset(d,-1,sizeof(d));
+    cout << solve (1,1);
     
-    printf("%d\n" ,go ( 1 , 1) ) ;
 }
-
