8c255e1c6ab5d590d28f9b33dc144ab6b0a6e47b
nisroeld99 <hellowworld1990@gmail.com>
2017-02-21 20:03:35 +0900

    acc

--- 1504/1504.cpp
+++ 1504/1504.cpp
@@ -1,67 +1,70 @@
-#include <algorithm>
 #include <iostream>
+#include <cstring>
 #include <vector>
 #include <queue>
 using namespace std;
-const int inf =987654321;
-typedef pair<int,int> ii;
+typedef pair<int, int> ii;
 typedef pair<int, ii> iii;
+typedef long long ll ;
 
+const int inf = 987654321;
+int n , m ;
+ll d[888][888];
+vector<ii>v[888];
+bool visit[888];
+priority_queue<ii,vector<ii>,greater<ii> > pq;
 
-
-vector<ii >g[888];
-
-long long d[888][888];
-priority_queue<ii,vector<ii>,greater<ii>>pq;
-void dijkstra(int start){
-    for (int i = 0 ; i<888;i++){
-        
-        d[start][i]=inf;
+void dijk(int start){
+    memset (visit, false ,sizeof(visit));
+    for (int i = 0 ; i<=n ; i++){
+        d[start][i] = inf ;
     }
-    d[start][start]=0;
-    pq.push(ii(0,start));
-    while (!pq.empty()){
-        ii temp = pq.top(); pq.pop();
-        int nowDist = temp.first;
+    d[start][start] = 0 ;
+    pq.push(ii(0 , start))  ;
+    
+    while(!pq.empty() ){
+        ii temp = pq.top();
+        int cost = temp.first;
         int now = temp.second;
-
-
-        for (int i = 0 ; i< g[now].size();i++){
-            int nextDist = nowDist + g[now][i].first;
-            int next = g[now][i].second;
-            if ( d[start][next]> nextDist ) {
-                d[start][next] =nextDist ;
-                pq.push(ii(nextDist,next));
+        pq.pop();
+        if (visit[now] )continue;
+        visit[now] = true;
+        for (auto x : v[now]){
+            int next_cost =x.first + cost;
+            int next = x.second;
+            
+            if ( d[start][next ] > next_cost){
+                d[start][next] = next_cost;
+                pq.push(ii( next_cost, next ) );
             }
         }
     }
 }
 
 int main(){
-    int n,m;
     scanf("%d%d",&n,&m);
-    
-    for (int i = 0 ; i<m;i++){
+    for (int i =0  ;i<m; i++){
         int from,to,cost;
         scanf("%d%d%d",&from,&to,&cost);
-        g[from].push_back(ii(cost,to));
-        g[to].push_back(ii(cost,from));
+        v[from].push_back(ii(cost,to));
+        v[to].push_back(ii(cost,from));
     }
+    int r1,r2;
+    scanf("%d%d",&r1,&r2);
+    
+    dijk (1);
+    dijk (r1);
+    dijk (r2);
+    
+    ll ans = min(d[1][r1] + d[r1][r2] + d[r2][n], d[1][r2] + d[r2][r1] + d[r1][n]);
+    
+    if (ans < inf )
+        printf("%lld\n", ans  );
+    else
+        printf("-1\n");
     
     
     
-    int f,s;
-    scanf("%d%d",&f,&s);
-    dijkstra(1);
-    dijkstra(f);
-    dijkstra(s);
-    
-    long long dab= min({d[1][f] +d[f][s]+ d[s][n] , d[1][s]+d[s][f]+d[f][n]});
-    if( dab <inf ){
-        cout << dab<<"\n";
-    }
-    else {
-        cout << -1<<"\n";
-    }
     
 }
+
